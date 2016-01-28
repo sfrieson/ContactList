be7f8693206660d518d878762632ddc996b78614
Steven Frieson <sfrieson@gmail.com>
2016-01-28 09:00:51 -0500

    Commit's Angular work on styling contacts

--- app/assets/javascripts/controllers/contacts_controller.js
+++ app/assets/javascripts/controllers/contacts_controller.js
@@ -1,5 +1,22 @@
 var ctrl = angular.module("ContactsController", []);
 
-ctrl.controller('contactController', ['$scope', function($scope){
+ctrl.controller('contactsController', ['$scope', '$http', function($scope, $http){
+    $scope.contacts=[];
+    $scope.newContact = {};
+
+    $scope.addContact = function(){
+        contact = $scope.newContact;
+        $http.post('/api/contacts', {contact: contact}).then(function(response){
+            dbContact = response.data;
+            $scope.contacts.push(data);
+        });
+    };
     
+    (function getContacts(){
+        $http.get("/api/contacts").then(function(response){
+            data = response.data;
+            console.log(data);
+            $scope.contacts = data.contacts;
+        });
+    })();
 }]);
--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,6 +1,19 @@
-class Api::ContactsController < ApplicationController
+    class Api::ContactsController < ApplicationController
     def index
         contacts = Contact.all
         render json: {contacts: contacts}
+    end
+    def create
+
+    end
+    def show
+        id = params.id
+        contact = Contact.find(id)
+        render json: contact
+    end
+
+    private
+    def contact_params
+        params.require(:contact).permit(:firstname, :lastname, :email, :username, :status, :img, :company, :phone)
     end
 end
